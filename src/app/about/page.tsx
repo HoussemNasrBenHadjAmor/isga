a69edb9af45b1a6d2981ede1b896a1c25efb1079
HoussemNasrBenHadjAmor <houssem.nasrbenhadjamor@essths.u-sousse.tn>
2024-11-10 00:03:05 -0500

    still working on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -34,7 +34,7 @@ const page = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 pb-20">
           {aboutCardImg.map((card) => (
             <CardImage
               key={card.id}
