cfecfcde42098c4fff1cc50f73220ab37e510fcc
HoussemNasrBenHadjAmor <houssem.nasrbenhadjamor@essths.u-sousse.tn>
2025-03-09 16:17:14 -0400

    Edited the client sanity

--- client/src/sanity/lib/client.ts
+++ client/src/sanity/lib/client.ts
@@ -2,10 +2,17 @@ import { createClient } from "next-sanity";
 
 // import { draftMode } from "next/headers";
 
+console.log(
+  process.env.VERCEL_URL === "production"
+    ? `${process.env.SANITY_PRODUCTION_URL}/studio`
+    : `${process.env.SANITY_LOCALE_URL}/studio`
+);
+
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: "2021-08-31",
+  // apiVersion: "2021-08-31",
+  apiVersion: "2025-03-09",
   useCdn: false,
   token: process.env.SANITY_API_READ_TOKEN,
   // perspective: draftModeResults.isEnabled ? "previewDrafts" : "published",
@@ -16,15 +23,25 @@ export const client = createClient({
 
     studioUrl:
       process.env.VERCEL_URL === "production"
-        ? `https://${process.env.PRODUCTION_URL}/studio`
-        : `${process.env.NEXT_PUBLIC_SANITY_LOCALE_URL}/studio`,
+        ? `${process.env.SANITY_PRODUCTION_URL}/studio`
+        : `${process.env.SANITY_LOCALE_URL}/studio`,
   },
 });
 
 export const edit_client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: "2021-08-31",
+  // apiVersion: "2021-08-31",
+  apiVersion: "2025-03-09",
   useCdn: false,
   token: process.env.SANITY_API_EDITOR_TOKEN,
+  stega: {
+    // enabled: (await draftMode()).isEnabled,
+    // enabled: true,
+
+    studioUrl:
+      process.env.VERCEL_URL === "production"
+        ? `${process.env.SANITY_PRODUCTION_URL}/studio`
+        : `${process.env.SANITY_LOCALE_URL}/studio`,
+  },
 });
