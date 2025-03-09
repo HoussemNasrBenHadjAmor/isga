a6a141815c84480f0d3f38e55cc747cad2835266
HoussemNasrBenHadjAmor <houssem.nasrbenhadjamor@essths.u-sousse.tn>
2025-03-08 22:37:52 -0500

    removed the apiVersion in sanity client

--- client/src/sanity/lib/client.ts
+++ client/src/sanity/lib/client.ts
@@ -5,7 +5,7 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  // apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  apiVersion: "2024-10-31",
   useCdn: false,
   token: process.env.SANITY_API_READ_TOKEN,
   // perspective: draftModeResults.isEnabled ? "previewDrafts" : "published",
